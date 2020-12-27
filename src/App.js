09a234b106e2d11f65a0108c205fe7f7a1f3a4e0
Reo <honohina0215@gmail.com>
2020-12-27 15:11:45 +0900

    delete App.css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Near_String from './near_string_.png';
-import './App.css';
 
 function App() {
   return (
-      <header className="flex justify-center flex-col items-center min-h-screen text-lg font-mono">
+      <div className="flex flex-col justify-center items-center min-h-screen text-lg font-mono">
         <img src={Near_String} className="h-60 mb-8" alt="Icon" />
-        <p className=" text-blueGray-800 text-3xl">This is pwa CRA template with Tailwindcss. </p>
-      </header>
+        <p className=" text-blueGray-800 text-3xl">This is pwa CRA template with Tailwindcss.</p>
+      </div>
   );
 }
 
