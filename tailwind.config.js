3cef9158f0225061adc60e1c647f4dbdbd4beb4e
Reo <honohina0215@gmail.com>
2020-12-27 14:59:45 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      white: colors.white,
+      black:colors.black,
+      blueGray: colors.blueGray,
+      coolGray: colors.coolGray,
+      gray: colors.gray,
+      trueGray: colors.trueGray,
+      warmGray: colors.warmGray,
+      red: colors.red,
+      orange: colors.orange,
+      amber: colors.amber,
+      yellow: colors.yellow,
+      lime: colors.lime,
+      green: colors.green,
+      emerald: colors.emerald,
+      teal:colors.teal,
+      cyan: colors.cyan,
+      lightBlue: colors.lightBlue,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      violet: colors.violet,
+      purple: colors.purple,
+      fuchsia: colors.fuchsia,
+      pink: colors.pink,
+      rose: colors.rose,
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
